ff8806d88489d668d87d3d15da9004f6b50e9729
dg015 <bruno.barbosa8@yahoo.com>
2024-03-13 19:49:52 -0400

    task 4b complete

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,19 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI selection;
+
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
         {
+            
             SelectedVillager.Selected(false);
+            
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+    }
+
+
+    public void Update()
+    {
+        GetVillager();
+    }
+
+    public void GetVillager()
+    {
+        if (SelectedVillager == null )
+        {
+
+            selection.text = "No selection";
+        }
+
+        else  if (SelectedVillager.CanOpen() == ChestType.Thief)
+        {
+            selection.text = "Thief";
+        }
+
+        else if (SelectedVillager.CanOpen() == ChestType.Archer)
+        {
+            selection.text = "Archer";
+        }
+
+        else if (SelectedVillager.CanOpen() == ChestType.Merchant)
+        {
+            selection.text = "Merchant";
+        }
+
+        else
+        {
+            selection.text = "No selection";
+        }
+        
+
     }
     
 }
