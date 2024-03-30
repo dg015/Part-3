97b1cbf01dff997d767cfe492a76e190604bb6c8
dg015 <bruno.barbosa8@yahoo.com>
2024-03-30 14:26:57 -0400

    Minigun working

--- Assets/Assignment/Script/Minigun.cs
+++ Assets/Assignment/Script/Minigun.cs
@@ -1,32 +1,93 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Minigun : Gun
 {
+    [SerializeField] private float Overheat;
+    [SerializeField] private float coolingdownTime;
+    [SerializeField] private float timeBetweenShots;
+    [SerializeField] private bool readyToFire = true;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
+        /*
+        if (Input.GetMouseButton(0)) 
+        {
+            overhearting();
+            nockback();
+        }
+        else
+        {
+            coolingDown();
+        }
+        */
         lookat();
-        nockback();
+        
+        if (Input.GetMouseButton(0) && readyToFire)
+        {
+            StartCoroutine(shootingCycle());
+        }
+        else
+        {
+            coolingDown();
+        }
+        
     }
+
+    private void overhearting()
+    {
+        if ( Input.GetMouseButton (0)) 
+        {
+            Overheat += Time.deltaTime;
+        }
+        
+        else if (Overheat == 6)
+        {
+            readyToFire = false;
+        }
+        if (Overheat < 6)
+        {
+            readyToFire = true;
+        }
+    }
+    private void coolingDown()
+    {
+        
+        if (Overheat > 0)
+        {
+            Overheat -= Time.deltaTime;
+        }
+
+    }
+
+
 
     protected override void nockback()
     {
-        if (Input.GetMouseButton(0))
-        {
-            player.AddForce(-1 * direction * explosionStrenght, ForceMode2D.Force);
-            shooot();
-        }
+        player.AddForce(-1 * direction * explosionStrenght, ForceMode2D.Force);
+        shooot();
     }
+    private IEnumerator shootingCycle()
+    {
+        
+        nockback();
+        overhearting();
+        readyToFire = false;
+        if (Overheat >= 6)
+        {
+            yield return new WaitForSeconds(5);
+        }
+
+        Debug.Log("I'me here");
+        readyToFire = true;
 
 
-
+    }
 
 }
