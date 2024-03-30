15bbddd308df5f0fd0c5060d402b9ed9f9f9d48d
dg015 <bruno.barbosa8@yahoo.com>
2024-03-30 14:58:48 -0400

    inventory system complete

---
+++ Assets/Assignment/Script/WeaponsSwitch.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsSwitch : MonoBehaviour
+{
+
+    [SerializeField] private GameObject gun;
+    [SerializeField] private GameObject sniper;
+    [SerializeField] private GameObject mingun;
+    [SerializeField] private int currentGun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Switch();
+        holdingGun();
+    }
+
+    private void holdingGun()
+    {
+        if (currentGun ==1)
+        {
+            gun.SetActive(true);
+            sniper.SetActive(false);
+            mingun.SetActive(false);
+        }
+        else if( currentGun ==2)
+        {
+            gun.SetActive(false);
+            sniper.SetActive(true);
+            mingun.SetActive(false);
+
+        }
+        else if (currentGun ==3)
+        {
+            gun.SetActive(false);
+            sniper.SetActive(false);
+            mingun.SetActive(true);
+        }
+
+
+
+    }
+
+    private void Switch()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f )
+        {
+
+            currentGun ++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+
+            currentGun++;
+        }
+        if( currentGun > 3)
+        {
+            currentGun = 1;
+        }
+        if (currentGun <1)
+        {
+            currentGun = 3;
+        }
+
+    }
+}
